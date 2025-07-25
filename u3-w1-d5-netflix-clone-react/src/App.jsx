c835cd6e21b01beee80e78e8dd7668d7b9bdbfa5
Dade1991 <dade91@msn.com>
2025-07-25 13:26:24 +0200

    Other changes

--- u3-w1-d5-netflix-clone-react/src/App.jsx
+++ u3-w1-d5-netflix-clone-react/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <CustomNavBar />
       <MainMenu />
-      <MovieCarousel />
+      <MovieCarousel SectionTitle="" />
     </>
   )
 }
--- u3-w1-d5-netflix-clone-react/src/components/MovieCarousel.jsx
+++ u3-w1-d5-netflix-clone-react/src/components/MovieCarousel.jsx
@@ -1,77 +1,81 @@
 import Carousel from "react-bootstrap/Carousel"
+import Container from "react-bootstrap/esm/Container"
 
 function MovieCarousel() {
   return (
-    <Carousel data-bs-theme="dark" className="mt-5">
-      <Carousel.Item>
-        <div className="d-flex flex-row">
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=f5f5f5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="d-flex flex-row">
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=f5f5f5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-            alt="First slide"
-          />
-        </div>
-      </Carousel.Item>
-    </Carousel>
+    <Container className="mt-5">
+      <h2>Trending Now</h2>
+      <Carousel data-bs-theme="dark" className="mt-5">
+        <Carousel.Item>
+          <div className="d-flex flex-row">
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=f5f5f5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="d-flex flex-row">
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=f5f5f5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+              alt="First slide"
+            />
+          </div>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
   )
 }
 

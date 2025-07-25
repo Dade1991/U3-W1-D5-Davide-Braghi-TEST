31c1739366e7c4d5a3661dfdc1e62d79e4863783
Dade1991 <dade91@msn.com>
2025-07-25 10:26:56 +0200

    Added folder & initialized the PJT

---
+++ u3-w1-d5-test-netflix-react-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import "./boostrap/dist/css/bootstrap.min.css"
+import "./components/CustomNavBar"
+
+function App() {
+  return (
+    <>
+      <CustomNavbar />
+    </>
+  )
+}
+
+export default App
